8d5f5c559fc445f5d1b60bb8fc8cd4a780122a75
victor <unicoreptiles@gmail.com>
2020-05-11 00:27:55 -0500

    aver

--- src/database.js
+++ src/database.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://unico-reptiles:unico-reptiles@cluster0-shard-00-00-qb2n1.mongodb.net:27017,cluster0-shard-00-01-qb2n1.mongodb.net:27017,cluster0-shard-00-02-qb2n1.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://unico-reptiles:unico-reptiles@cluster0-qb2n1.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true
 })
 
   .then(db => console.log('Base de datos conectada'))
   .catch(err => console.log(err));
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://unico-reptiles:unico-reptiles@cluster0-qb2n1.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+  

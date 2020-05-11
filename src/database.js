a711d3e956ddbe73e513082f460ee08c6da98cf8
victor <unicoreptiles@gmail.com>
2020-05-11 00:13:23 -0500

    hola

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://unico-reptiles:unico-reptiles@cluster0-qb2n1.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb://unico-reptiles:unico-reptiles@cluster0-shard-00-00-qb2n1.mongodb.net:27017,cluster0-shard-00-01-qb2n1.mongodb.net:27017,cluster0-shard-00-02-qb2n1.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
   useNewUrlParser: true
 })
 

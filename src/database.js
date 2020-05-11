ec913a7952db40d5c39f9e26358279761666f850
victor <unicoreptiles@gmail.com>
2020-05-11 00:07:29 -0500

    aver

--- src/database.js
+++ src/database.js
@@ -3,5 +3,6 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://unico-reptiles:unico-reptiles@cluster0-qb2n1.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true
 })
+
   .then(db => console.log('Base de datos conectada'))
   .catch(err => console.log(err));

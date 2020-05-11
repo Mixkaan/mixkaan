130a799da6a91511c4f5c3f8b61ae211b68566f4
victor <unicoreptiles@gmail.com>
2020-05-10 22:15:22 -0500

    primer commit

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://unico-reptiles:Elizabeth123@cluster0-qb2n1.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://unico-reptiles:unico-reptiles@cluster0-qb2n1.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true
 })
   .then(db => console.log('Base de datos conectada'))

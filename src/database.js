715e0d3622cc89b680d788ec4aed0d1b147ad279
victor <unicoreptiles@gmail.com>
2020-05-11 01:10:14 -0500

    s

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://unico-reptiles:<password>@unico-qb2n1.mongodb.net/test?retryWrites=true&w=majority', {
+const uri = 'mongodb+srv://unico-reptiles:<password>@unico-qb2n1.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(uri, {
   useNewUrlParser: true
 })
 

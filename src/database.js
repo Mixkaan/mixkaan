dca5b4c1e7f0432b53bf05fea284551a26bb7cb4
victor <unicoreptiles@gmail.com>
2020-05-11 00:51:10 -0500

    s

--- src/database.js
+++ src/database.js
@@ -1,19 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://unico-reptiles:unico-reptiles@cluster0-qb2n1.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://unico-reptiles:<password>@unico-qb2n1.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true
 })
 
   .then(db => console.log('Base de datos conectada'))
   .catch(err => console.log(err));
-
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://unico-reptiles:unico-reptiles@cluster0-qb2n1.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-  

691685b1f659ef91704b8e314b5d661e08c3b17b
mixkaan <mixkaan079@gmail.com>
2020-06-28 19:51:11 -0500

    node-sass-middleware agregado

--- src/index.js
+++ src/index.js
@@ -17,6 +17,14 @@ app.set('port', process.env.PORT || 7000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//SASS-MIDDLEWARE
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentSyntax: true,
+  sourceMap: true
+}));
+
 //MIDDLEWARES
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));

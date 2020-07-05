a0d88334ad23bd384cdf17ebd77cccfaa1c3c0c9
gaitto079 <unicoreptiles@gmail.com>
2020-07-05 14:32:28 -0500

    agregadas mas fotos

--- src/database.js
+++ src/database.js
@@ -1,14 +1,14 @@
 //-----BASE-DE-DATOS-PRODUCCION-----//
-/* const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 mongoose.connect('mongodb://unico-reptiles:unico123@ds231758.mlab.com:31758/heroku_5df7bcgr', {
   useNewUrlParser: true
-}); */
+});
 
 //-----BASE-DE-DATOS-DESARROLLO-----//
-const mongoose = require('mongoose');
+/* const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/unico', {
   useNewUrlParser: true
 })
 
   .then(db => console.log('Base de datos conectada'))
-  .catch(err => console.log(err));
+  .catch(err => console.log(err)); */
